d645891993bdd8a32263fad1f34ce23624a90fa8
AlejandroDeheza <ale.deheza@gmail.com>
2021-04-27 20:13:13 -0300

    Arreglo bug en clase Usuario

--- src/main/java/ropa/Usuario.java
+++ src/main/java/ropa/Usuario.java
@@ -1,10 +1,11 @@
 package ropa;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Usuario {
 
-  private List<Uniforme> sugerencias;
+  private List<Uniforme> sugerencias = new ArrayList<>();
 
   public void agregarSugerencia(Uniforme sugerencia){
     sugerencias.add(sugerencia);
